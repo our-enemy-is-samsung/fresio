421cf9dcc65cb45d8cb5b83fa4094b214cf3d496
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-10-24 01:04:00 +0900

    feat: 프로젝트 텍스트 스타일 구조 추가

---
+++ shared/enums/TextSize.ts
@@ -0,0 +1,11 @@
+export enum TextSize {
+    TitleLarge = 34,
+    TitleMedium = 28,
+    TitleSmall = 23,
+    HeadingLarge = 21,
+    HeadingSmall = 19,
+    BodyLarge = 16,
+    BodySmall = 14,
+    LabelLarge = 12,
+    LabelSmall = 11,
+}
