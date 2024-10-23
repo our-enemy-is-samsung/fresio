bfc051373972362bb0d4a8a4b12036db376bf385
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-10-24 01:39:56 +0900

    feat: View 컴포넌트 추가

---
+++ components/atoms/View.tsx
@@ -0,0 +1,24 @@
+// components/StyledView.tsx
+import {useColorScheme, View, ViewStyle} from 'react-native';
+import {FC, ReactNode} from 'react';
+import {Colors} from "@/shared/constants/Color";
+
+interface ViewProps {
+	testID?: string;
+	backgroundColor?: keyof typeof Colors.light & keyof typeof Colors.dark;
+	children?: ReactNode;
+	style?: ViewStyle;
+	isSafeArea?: boolean;
+}
+
+const StyledView: FC<ViewProps> = ({testID, backgroundColor = 'grayScale.primary5', children, style}) => {
+	const colorScheme = useColorScheme() ?? 'light';
+	const viewStyle: ViewStyle = {
+		backgroundColor: Colors[colorScheme][backgroundColor],
+		...style,
+	};
+
+	return <View testID={testID} style={viewStyle}>{children}</View>;
+};
+
+export default StyledView;
