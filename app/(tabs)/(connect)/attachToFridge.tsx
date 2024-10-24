891d01dba7a3d5fe3a9e6dd94fabaf7dbbb053e7
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-10-24 11:41:27 +0900

    feat: attachToFridge.tsx 페이지 추가

---
+++ app/(tabs)/(connect)/attachToFridge.tsx
@@ -0,0 +1,53 @@
+import Text from "@/components/atoms/Text";
+import {TextSize} from "@/shared/enums/TextSize";
+import {Dimensions, SafeAreaView, StyleSheet} from "react-native";
+import View from "@/components/atoms/View";
+import ConnectHeader from "@/feature/connect/ui/ConnectHeader";
+import Button from "@/components/atoms/Button";
+import {ButtonSize, ButtonStyle} from "@/shared/types/Button";
+
+const PageBluetoothRequired = () => {
+	return (
+		<SafeAreaView style={{flex: 1}}>
+			<ConnectHeader/>
+			<View style={styles.container}>
+				<View style={styles.textContainer}>
+					<Text size={TextSize.TitleSmall} color={'grayScale100'}>거의 다 되었습니다...</Text>
+					<Text size={TextSize.HeadingSmall} color={'grayScale60'} textAlign={'center'}>
+						Fresio 기기를 냉장고에 부착해주세요
+					</Text>
+				</View>
+			</View>
+			<View style={styles.buttonContainer}>
+				<Button
+					radius={9999}
+					style={ButtonStyle.Primary}
+					size={ButtonSize.Small}
+					buttonStyles={{paddingHorizontal: 36}}
+				>다음</Button>
+			</View>
+		</SafeAreaView>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+
+		paddingTop: 42,
+	},
+	textContainer: {
+		alignItems: 'center',
+
+		gap: 20,
+	},
+	buttonContainer: {
+		flexDirection: 'row-reverse',
+		justifyContent: 'space-between',
+		gap: 12,
+
+		paddingHorizontal: 20,
+	}
+})
+
+export default PageBluetoothRequired;
