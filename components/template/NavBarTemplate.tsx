045b796f096bb439745ad12f4a5ccfea51e0df9e
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-03 19:08:00 +0900

    refactor: NavBarTemplate.tsx의 음식을 냉장고 음식으로 변경

--- components/template/NavBarTemplate.tsx
+++ components/template/NavBarTemplate.tsx
@@ -5,7 +5,7 @@ const NavBarTemplate = () => {
 	return (
 		<NavBar>
 			<NavBar.Item icon={<Feather name="home"/>} title={'홈'} selected={true} onClick={() => console.log('asd')}/>
-			<NavBar.Item icon={<Feather name="grid"/>} title={'음식'} selected={false} onClick={() => console.log('asd')}/>
+			<NavBar.Item icon={<Feather name="grid"/>} title={'냉장고 음식'} selected={false} onClick={() => console.log('asd')}/>
 		</NavBar>
 	)
 }
