e5f48b77590f2a01e6da5bf59b189dd8a55a1b56
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-03 20:23:06 +0900

    fix: NavBarTemplate.tsx에 클릭시 navigation 추가

--- components/template/NavBarTemplate.tsx
+++ components/template/NavBarTemplate.tsx
@@ -1,11 +1,16 @@
 import NavBar from "@/components/molecules/NavBar";
 import Feather from "@expo/vector-icons/Feather";
+import {useNavigation} from "expo-router";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {ParamListBase} from "@react-navigation/native";
 
 const NavBarTemplate = () => {
+	const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
 	return (
 		<NavBar>
-			<NavBar.Item icon={<Feather name="home"/>} title={'홈'} selected={true} onClick={() => console.log('asd')}/>
-			<NavBar.Item icon={<Feather name="grid"/>} title={'냉장고 음식'} selected={false} onClick={() => console.log('asd')}/>
+			<NavBar.Item icon={<Feather name="home"/>} title={'홈'} selected={true} onClick={() => navigation.navigate('(tabs)/index')}/>
+			<NavBar.Item icon={<Feather name="grid"/>} title={'냉장고 음식'} selected={false} onClick={() => navigation.navigate('(tabs)/(food)/index')}/>
 		</NavBar>
 	)
 }
