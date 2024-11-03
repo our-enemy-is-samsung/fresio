bdf7db1e298108abd60073ef33e5578ab731b832
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-03 19:17:44 +0900

    feat: 냉장고 음식 뼈대 추가

---
+++ app/(tabs)/(food)/index.tsx
@@ -0,0 +1,34 @@
+import View from "@/components/atoms/View";
+import {SafeAreaView, ScrollView, StyleSheet} from "react-native";
+import PageHeader from "@/components/molecules/PageHeader";
+import StyledText from "@/components/atoms/Text";
+import {TextSize} from "@/shared/enums/TextSize";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+
+const PageFood = () => {
+	const safeAreaInsets = useSafeAreaInsets();
+	return (
+		<View style={{flex: 1, paddingTop: safeAreaInsets.top}}>
+			<PageHeader name={'냉장고 음식'} />
+			<ScrollView style={styles.content}>
+				<View style={styles.contentHeader}>
+					<StyledText size={TextSize.BodySmall} color={'grayScale40'}>음식 38개</StyledText>
+				</View>
+			</ScrollView>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	content: {
+		marginTop: 32,
+	},
+	contentHeader: {
+
+	}
+})
+
+export default PageFood;
