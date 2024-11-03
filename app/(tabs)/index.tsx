a9e0eb90703ce9178c5116654f622f61a798c862
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-03 16:24:29 +0900

    refactor: index 페이지를 제외한 모든 라우터 제거

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,18 @@
-import {StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import PageHeader from "@/components/molecules/PageHeader";
+import StyledText from "@/components/atoms/Text";
+import {TextSize} from "@/shared/enums/TextSize";
+import View from "@/components/atoms/View";
+import React from "react";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+const HomeScreen = () => {
+	const safeAreaInsets = useSafeAreaInsets();
 	return (
-		<>
-		</>
+		<View style={{flex: 1, paddingTop: safeAreaInsets.top}}>
+			<PageHeader name={'홈'} />
+		</View>
 	);
 }
+
+export default HomeScreen;
