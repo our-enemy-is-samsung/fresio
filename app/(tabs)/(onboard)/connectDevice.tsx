f15aaef5806658de343aa99ad551856ee16b4f43
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-04 08:57:25 +0900

    feat: connectDevice.tsx 페이지에 onboardBlurGradient 추가

--- app/(tabs)/(onboard)/connectDevice.tsx
+++ app/(tabs)/(onboard)/connectDevice.tsx
@@ -5,14 +5,16 @@ import {TextSize} from "@/shared/enums/TextSize";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import OnboardNextHeader from "@/feature/onboard/ui/onboardNextHeader";
 import OnboardTitle from "@/feature/onboard/ui/onboardTitle";
+import OnboardBlurGradient from "@/feature/onboard/ui/onboardBlurGradient";
 
 const PageConnectDevice = () => {
 	const safeAreaInsets = useSafeAreaInsets();
 	return (
-		<SafeAreaView style={{marginTop: safeAreaInsets.top}}>
+		<SafeAreaView style={{marginTop: safeAreaInsets.top, flex: 1}}>
 			<OnboardNextHeader />
-			<OnboardTitle title={'기기 연결'} description={'프레시오의 페어링 버튼을 3초 이상 눌러주세요\n' +
+			<OnboardTitle title={'제품 연결 준비'} description={'프레시오의 페어링 버튼을 3초 이상 눌러주세요\n' +
 				'연결이 준비되면 기기에서 소리가 재생됩니다'} />
+			<OnboardBlurGradient />
 		</SafeAreaView>
 	)
 }
