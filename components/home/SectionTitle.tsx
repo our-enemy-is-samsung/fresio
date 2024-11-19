7fe8280c76c3dc502e1ca348c1ce54b71700d7c7
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-20 08:21:53 +0900

    SectionTitle.tsx에 더보기 버튼 추가

--- components/home/SectionTitle.tsx
+++ components/home/SectionTitle.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import StyledText from "@/components/shared/Text";
 import {TextSize} from "@/enums/TextSize";
+import {StyleSheet} from "react-native";
+import {Row} from "@/components/shared/Row";
 
-const SectionTitle = ({title}: {title: string}) => {
+const SectionTitle = ({title, showMoreButton}: {title: string, showMoreButton?: boolean}) => {
 	return (
-		<StyledText size={TextSize.HeadingSmall} color={'content'} style={{marginHorizontal: 22}}>{title}</StyledText>
+		<Row style={styles.container}>
+			<StyledText size={TextSize.BodyLarge} color={'content'}>{title}</StyledText>
+			{showMoreButton && <StyledText size={TextSize.BodySmall} color={'contentDim'} style={{textDecorationLine: 'underline'}}>더보기</StyledText>}
+		</Row>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		justifyContent: 'space-between',
+
+		paddingHorizontal: 22,
+	}
+})
 
 export default SectionTitle;
