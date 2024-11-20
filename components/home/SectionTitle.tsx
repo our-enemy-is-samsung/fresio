c19e0637c3b4bf1b39e008ec7108d81383153b5d
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-20 21:35:01 +0900

    SectionTitle에 titleColor props 추가

--- components/home/SectionTitle.tsx
+++ components/home/SectionTitle.tsx
@@ -3,12 +3,27 @@ import StyledText from "@/components/shared/Text";
 import {TextSize} from "@/enums/TextSize";
 import {StyleSheet, ViewStyle} from "react-native";
 import {Row} from "@/components/shared/Row";
+import {MaterialIcons} from "@expo/vector-icons";
+import {Colors} from "@/constants/Color";
 
-const SectionTitle = ({title, showMoreButton, style}: {title: string, showMoreButton?: boolean, style: ViewStyle}) => {
+interface SectionTitleProps {
+	title: string;
+	showMoreButton?: boolean;
+	style?: ViewStyle;
+
+	titleColor?: keyof typeof Colors;
+}
+
+const SectionTitle = ({title, showMoreButton, style, titleColor = 'content'}: SectionTitleProps) => {
 	return (
 		<Row style={{...styles.container, ...style}}>
-			<StyledText size={TextSize.HeadingSmall} color={'content'}>{title}</StyledText>
-			{showMoreButton && <StyledText size={TextSize.BodySmall} color={'contentDim'} style={{textDecorationLine: 'underline'}}>더보기</StyledText>}
+			<StyledText size={TextSize.BodyLarge} color={titleColor}>{title}</StyledText>
+			{showMoreButton && (
+				<Row>
+					<StyledText size={TextSize.BodySmall} color={'brand'} style={{textDecorationLine: 'underline'}}>더보기</StyledText>
+					<MaterialIcons name="keyboard-arrow-right" size={24} color={Colors['brand']} />
+				</Row>
+			)}
 		</Row>
 	)
 }
@@ -18,7 +33,7 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 
 		paddingHorizontal: 22,
-	}
+	},
 })
 
 export default SectionTitle;
