f3e834d8343472f401f9030981cd457f3f6fe135
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-11-21 00:19:06 +0900

    NavBarTemplate.tsx selected에 라우팅 연결

--- components/template/NavBarTemplate.tsx
+++ components/template/NavBarTemplate.tsx
@@ -2,16 +2,19 @@
 import Feather from "@expo/vector-icons/Feather";
 import {useNavigation} from "expo-router";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {ParamListBase} from "@react-navigation/native";
+import {ParamListBase, useRoute} from "@react-navigation/native";
 import NavBar from "@/components/shared/NavBar";
 
 const NavBarTemplate = () => {
 	const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+	const currentRoute = useRoute().name;
+
+	console.log(currentRoute);
 
 	return (
 		<NavBar>
-			<NavBar.Item icon={<Feather name="home"/>} title={'홈'} selected={true} onClick={() => navigation.navigate('index')}/>
-			<NavBar.Item icon={<Feather name="grid"/>} title={'냉장고 음식'} selected={false} onClick={() => navigation.navigate('food/index')}/>
+			<NavBar.Item icon={<Feather name="home"/>} title={'홈'} selected={currentRoute === 'index'} onClick={() => navigation.navigate('index')}/>
+			<NavBar.Item icon={<Feather name="grid"/>} title={'냉장고 음식'} selected={currentRoute === 'food/index'} onClick={() => navigation.navigate('food/index')}/>
 		</NavBar>
 	)
 }
